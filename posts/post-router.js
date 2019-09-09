da21912667a755e9c8beb80f9811f8a9d9b8e12c
Elvis Knapman <elvis@elvisknapman.com>
2019-09-09 14:07:14 -0400

    practice

--- posts/post-router.js
+++ posts/post-router.js
@@ -1,28 +1,79 @@
-const express = require('express');
+const express = require("express");
 
 // database access using knex
-const db = require('../data/db-config.js');
+const db = require("../data/db-config.js");
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
+  // gte data from database and return it to the client
 
+  // all database operations return a promise
+  db("posts")
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(err => {
+      console.log(err);
+      res.json(err);
+    });
 });
 
-router.get('/:id', (req, res) => {
-
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  db("posts")
+    .where({ id: id })
+    .first()
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(err => {
+      res.json(err);
+    });
 });
 
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
+  const postData = req.body;
+  // validate data before inserting
 
+  // then insert
+  db("posts")
+    .insert(postData, "id")
+    .then(([id]) => {
+      res.status(200).json(id);
+    })
+    .catch(err => {
+      res.json(err);
+    });
 });
 
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
+  const { id } = req.params;
+  const changes = req.body;
 
+  db("posts")
+    .where({ id })
+    .update(changes)
+    .then(count => {
+      res.status(200).json({ message: `Updated ${count} records` });
+    })
+    .catch(err => {
+      res.json(err);
+    });
 });
 
-router.delete('/:id', (req, res) => {
-
+router.delete("/:id", (req, res) => {
+  // delete posts from where ...
+  const { id } = req.params;
+  db("posts")
+    .where({ id: id })
+    .del()
+    .then(count => {
+      res.status(200).json({ message: `deleted ${count} records` });
+    })
+    .catch(err => {
+      res.json(err);
+    });
 });
 
 module.exports = router;
